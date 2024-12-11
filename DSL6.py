f503326e25229ec1687795469cfe9a464e54abe3
Ayush312004 <ayushlonare454@gmail.com>
2024-12-11 07:30:03 +0530

    Update DSL6.py

--- DSL6.py
+++ DSL6.py
@@ -8,7 +8,7 @@ def selection_sort(a):
     for i in range(len(a) - 1):
         min_index = i
         for j in range(i, len(a)):
-            if a[i] > a[j]:
+            if a[min_index] > a[j]:
                 min_index = j
         a[i], a[min_index] = a[min_index], a[i]
         
